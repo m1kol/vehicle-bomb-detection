53f989b6eecdf87e2b05cc51f64ab2f0bce5e4c6
Mihail Kolesov <kolesov.ma@phystech.edu>
2022-02-03 13:34:57 +0300

    Updated frames_convertion module: added specific functions that could be used instaed of a class.

--- src/frames_convertion.py
+++ src/frames_convertion.py
@@ -49,10 +49,53 @@ class FrameConverter:
             frames = frames[::step]
         
         converted_frames = []
-        for i in range(len(frames) - compute_window):
+        for i in range(len(frames) - compute_window + 1):
             if self.convert_type == "freq":
                 converted_frames.append(self._get_frequency_difference(frames[i:i+compute_window], self.epsilon))
             elif self.convert_type == "ampl":
                 converted_frames.append(self._get_amplitude_difference(frames[i:i+compute_window]))
 
         return converted_frames
+
+
+def get_frequency_diff(frames, epsilon: int):
+    interframe_diffs = np.array(
+        [
+            np.abs(frames[i+1] - frames[i]) > epsilon
+            for i in range(len(frames) - 1)
+        ]
+    )
+
+    return 255 * interframe_diffs.mean(axis=0).astype(np.uint8)
+
+def get_aplitude_diff(frames):
+    interframe_diffs = np.array(
+        [
+            np.abs(frames[i + 1] - frames[i])
+            for i in range(len(frames) - 1)
+        ]
+    )
+
+    return interframe_diffs.mean(axis=0).astype(np.uint8)
+
+
+def frequency_conversion(frames, epsilon, compute_window: int = 2, step: int = 1):
+    if step > 1:
+        frames = frames[::step]
+
+    converted_frames = []
+    for i in range(len(frames) - compute_window + 1):
+        converted_frames.append(get_frequency_diff(frames[i:i+compute_window], epsilon))
+
+    return converted_frames
+
+
+def amplitude_conversion(frames, compute_window: int = 2, step: int = 1):
+    if step > 1:
+        frames = frames[::step]
+
+    converted_frames = []
+    for i in range(len(frames) - compute_window + 1):
+        converted_frames.append(get_aplitude_diff(frames[i:i+compute_window]))
+
+    return converted_frames
